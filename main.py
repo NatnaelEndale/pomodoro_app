7a818b42705c89821941e236d7a501a020158f3d
Natnael Endale Cheru <natnaelendale22@gmail.com>
2025-12-26 23:30:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+
+# ------------------CONSTANTS-------------------#
+PINK = "#e2979c"
+RED = "e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ------------------ TIMER RESET -----------------#
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text= "00:00")
+    title.config(text = "Timer", fg=GREEN)
+    check_mark.config(text="")
+
+
+
+# ------------------ TIMER MECHANISM --------------#
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title.config(text= "Long Break", fg= RED)
+    elif reps % 2 != 0:
+        count_down(work_sec)
+        title.config(text= "Work", fg= GREEN)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
+
+# ------------------ COUNTDOWN MECHANISM----------------#
+def count_down(count):
+    global reps
+    count_min = count // 60
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = reps // 2
+        for _ in range(work_sessions):
+            mark += "✔"
+        check_mark.config(text= mark)
+
+
+
+# ------------------ UI SETUP -------------------#
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width=300, height=300, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file= "tomato3.png")
+canvas.create_image(150, 150, image=tomato_img)
+timer_text = canvas.create_text(150, 170, text='00:00', fill="white", font=(FONT_NAME, 35, 'bold'))
+canvas.grid(row=1, column=1)
+
+
+check_mark = Label(font=("Courier", 23, "bold"), fg=GREEN, bg=YELLOW)
+check_mark.grid(row= 3, column= 1)
+check_mark.config(padx= 10, pady=10)
+
+title = Label(text= "Timer", font=("Courier", 50, "bold"), fg=GREEN, bg=YELLOW)
+title.grid(row= 0, column= 1)
+title.config(padx= 10, pady=10)
+
+start_button = Button(text= "Start", highlightthickness=0, command=start_timer)
+start_button.grid(row=2, column= 0)
+start_button.config(padx= 10, pady=5)
+
+reset_button = Button(text= "Reset", highlightthickness=0, command= reset_timer)
+reset_button.grid(row=2, column= 2)
+reset_button.config(padx= 10, pady=5)
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
